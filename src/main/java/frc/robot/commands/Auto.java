3d6663d90197f7994e3029319eca5bece8c4e510
Charles Bruel <csbruel@gmail.com>
2023-03-04 09:29:53 -0500

    Auto sketch

---
+++ src/main/java/frc/robot/commands/Auto.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class Auto extends SequentialCommandGroup {
+  /** Creates a new Auto. */
+  public Auto() {
+    addCommands(
+      //Grab
+      new TestPneumatic(false, true),
+      //Wait
+      //Move arm
+      //retract
+      //move arm
+      //release
+      //drive robot?
+    );
+  }
+}
