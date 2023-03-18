044c22bc546b98159fa3f7b4805f4546fa35247c
Charles Bruel <csbruel@gmail.com>
2023-03-18 06:29:55 -0400

    Auto chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,8 +10,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.*;
 import frc.robot.commands.*;
@@ -43,6 +41,10 @@ public class RobotContainer {
   public static JoystickButton operatorYButton = new JoystickButton(operator, Constants.kButtonY);
 
   public RobotContainer() {
+    Autos.autoChooser.setDefaultOption("Mid Cone, Mobility", Autos.midConeMove);
+    Autos.autoChooser.addOption("Mid Cone", Autos.midCone);
+    Autos.autoChooser.addOption("None", null);
+
     compressor.enableDigital();
 
     drivetrain.setDefaultCommand(new DriveArcade());
@@ -78,18 +80,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return new SequentialCommandGroup(
-      new ArmPID(Constants.kAutoArmPos),
-      new Delay(Constants.kAutoPrereleaseTime),
-      new Pneumatic(false),
-      new Delay(Constants.kAutoPostreleaseTime),
-      new ParallelCommandGroup(
-        new SequentialCommandGroup(
-          new ArmPID(Constants.kArmRetracted),
-          new Pneumatic(true)
-        ),
-        new DriveTime(Constants.kAutoDriveTime, Constants.kAutoDrivePower, 0)
-      )
-    );
+    return Autos.autoChooser.getSelected();
   }
 }
---
+++ src/main/java/frc/robot/commands/Autos.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+
+public class Autos {
+    public static final Command midConeMove = new SequentialCommandGroup(
+        new ArmPID(Constants.kAutoArmPos),
+        new Delay(Constants.kAutoPrereleaseTime),
+        new Pneumatic(false),
+        new Delay(Constants.kAutoPostreleaseTime),
+        new ParallelCommandGroup(
+            new SequentialCommandGroup(
+                new ArmPID(Constants.kArmRetracted),
+                new Pneumatic(true)
+            ),
+            new DriveTime(Constants.kAutoDriveTime, Constants.kAutoDrivePower, 0)
+        )
+    );
+    
+    public static final Command midCone = new SequentialCommandGroup(
+        new ArmPID(Constants.kAutoArmPos),
+        new Delay(Constants.kAutoPrereleaseTime),
+        new Pneumatic(false),
+        new Delay(Constants.kAutoPostreleaseTime),
+        new ArmPID(Constants.kArmRetracted),
+        new Pneumatic(true)
+    );
+
+    public static final SendableChooser<Command> autoChooser = new SendableChooser<>();
+}
