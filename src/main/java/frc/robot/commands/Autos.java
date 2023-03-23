c2c756d73183cf334f4453ee8866aa09c9ec2810
SunayAgarwal <96151207+SunayAgarwal@users.noreply.github.com>
2023-03-23 17:28:37 -0400

    Update Autos.java

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
+import 
 
 public class Autos {
     public static final Command midConeMove = new SequentialCommandGroup(
@@ -29,6 +30,20 @@ public class Autos {
         new ArmPID(Constants.kArmRetracted),
         new Pneumatic(true)
     );
+    
+    public static final Command midConeBalance = new SequentialCommandGroup(
+        new ArmPID(Constants.kAutoArmPos),
+        new Delay(Constants.kAutoPrereleaseTime),
+        new Pneumatic(false),
+        new Delay(Constants.kAutoPostreleaseTime),
+        new ParallelCommandGroup(
+            new SequentialCommandGroup(
+                new ArmPID(Constants.kArmRetracted),
+                new Pneumatic(true)
+            ),
+            new DriveTime(Constants.kAutoDriveTime, Constants.kAutoDrivePower, 0)
+        )
+        new DriveTime(Constants.kAutoDriveTime / 2, -1 * Constants.kAutoDrivePower, 0)
 
     public static final SendableChooser<Command> autoChooser = new SendableChooser<>();
 }
