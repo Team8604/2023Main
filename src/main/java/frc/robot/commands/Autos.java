1848854a8cd2adb7dca9c125c6b545ca922a1706
SunayAgarwal <96151207+SunayAgarwal@users.noreply.github.com>
2023-03-25 00:13:22 -0400

    Rough draft
    highly doubt it will work, but it is an attempt at a working balance

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import frc.robot.Constants;
 
 public class Autos {
@@ -35,14 +36,15 @@ public class Autos {
         new Delay(Constants.kAutoPrereleaseTime),
         new Pneumatic(false),
         new Delay(Constants.kAutoPostreleaseTime),
-        new ParallelCommandGroup(
-            new SequentialCommandGroup(
-                new ArmPID(Constants.kArmRetracted),
-                new Pneumatic(true)
-            ),
-            new DriveTime(Constants.kAutoDriveTime, Constants.kAutoDrivePower, 0)
-        ),
-        new DriveTime(Constants.kAutoDriveTime / 2, -1 * Constants.kAutoDrivePower, 0)
+        new ArmPID(Constants.kArmRetracted),
+        new Pneumatic(true)
+        while (Robot.gyro.getAngle < 5 && Robot.gyro.getAngle > -5) {
+            RobotContainer.drivetrain.set(.35,0);
+        },
+        while (Robot.gyro.getAngle > 10) {
+            RobotContainer.drivetrain.set(0.1,0);
+        },
+        RobotContainer.drivetrain.set(0,0)
     );
 
     public static final SendableChooser<Command> autoChooser = new SendableChooser<>();
